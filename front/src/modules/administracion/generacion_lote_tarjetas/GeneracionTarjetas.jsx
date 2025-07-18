d2079e3f769b5e0aa7d2016d4b73d21cce4f49dc
Renato Hacel CR <renato.calymayor@zapopan.gob.mx>
2025-07-18 15:00:09 -0600

    ignore warning in file

--- front/src/modules/administracion/generacion_lote_tarjetas/GeneracionTarjetas.jsx
+++ front/src/modules/administracion/generacion_lote_tarjetas/GeneracionTarjetas.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import toast from "react-hot-toast"
 import Table from "../../ui/components/table/Table"
 import { useLocation } from "react-router-dom"
